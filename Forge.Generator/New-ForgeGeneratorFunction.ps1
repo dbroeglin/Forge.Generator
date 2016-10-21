f2e0d6ddc4a10c53fe7bdb6a11ec1e17a67ea5da
Dominique Broeglin <dominique.broeglin@gmail.com>
2016-10-22 01:09:02 +0200

    Added a generator for generator functions

---
+++ Forge.Generator/New-ForgeGeneratorFunction.ps1
@@ -0,0 +1,97 @@
+<#
+Copyright 2016 Dominique Broeglin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+function New-ForgeGeneratorFunction {
+    <#
+    .SYNOPSIS
+        Creates a new generator function in a generator module.
+
+    .DESCRIPTION
+        Creates a new skeleton generator function based on the arguments passed to the function.
+
+    .EXAMPLE
+        New-ForgeGeneratorFunction -Name MyFunction
+
+    .PARAMETER Name
+        The name of the new function.
+    
+    .PARAMETER Path
+        The path where the function should be generated.
+
+        Default: .\
+
+    .PARAMETER ModuleName
+        The name of the module in which the function is generated if it is not the
+        same as the parent directory name.
+
+    .PARAMETER Parameter
+        An array of parameter names to generate for the new function.
+
+    .PARAMETER DESCRIPTION
+        A very short description of the generated result. Should make sense in a
+        sentence like "<%= $Name %> generates <%= $Description %>"
+
+        Default: "TODO"
+    #>
+    [CmdletBinding(ConfirmImpact='Low')]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String]$Name,
+
+        [String]$Path = ".\",
+
+        [String]$ModuleName,
+
+        [String[]]$Parameter = @(),
+
+        [String]$Description = "TODO"
+    )
+    Begin {
+        Initialize-ForgeContext -SourceRoot (Join-Path $PSScriptRoot "Templates") `
+            -DestinationPath $Path
+
+        if (-not $ModuleName) {
+            $ModuleName = Split-Path -Leaf (Get-ForgeContext).DestinationPath
+        }
+    }
+    Process {
+        $ModulePath = Join-Path (Get-ForgeContext).DestinationPath $ModuleName
+        $PsdPath = Join-Path $ModulePath "$ModuleName.psd1"
+        if (-not (Test-Path -PathType Container $ModulePath)) {
+            throw "Module directory '$ModulePath' does not exist"
+        }
+        if (-not (Test-Path -PathType Container 'Tests')) {
+            throw "Test directory 'Tests' does not exist"
+        }
+        if (-not (Test-Path -PathType Leaf $PsdPath)) {
+            throw "PSD file '$PsdPath' does not exist"
+        }
+
+        Set-ForgeBinding @{
+            Name        = $Name
+            ModuleName  = $ModuleName
+            Parameters  = $Parameter
+            Description = $Description
+        }
+
+        $FunctionFilename = "$Name.ps1"
+        $TestsFilename    = "$Name.Tests.ps1"
+        Copy-ForgeFile -Source "Function.ps1" -Dest (Join-Path $ModuleName $FunctionFilename)
+        Copy-ForgeFile -Source "Function.Tests.ps1" -Dest (Join-Path Tests $TestsFilename)
+        Update-ModuleManifest -Path $PsdPath -FunctionsToExport (
+            (Import-PowerShellDataFile $PsdPath)["FunctionsToExport"] + $Name
+        )
+    }
+}
---
+++ Tests/New-ForgeGeneratorFunction.Tests.ps1
@@ -0,0 +1,85 @@
+Set-PSDebug -Strict
+$ErrorActionPreference = "Stop"
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+if (!(Get-Module Forge)) {
+    Import-Module "Forge"
+}
+. "$PSScriptRoot\..\Forge.Generator\New-ForgeGenerator.ps1"
+. "$PSScriptRoot\..\Forge.Generator\$sut"
+
+Describe "New-ForgeGeneratorFunction" {
+    $ModuleName = "TestModule"
+    $FunctionName = "TestFunction"
+    $TestBase   = Setup -Dir $ModuleName -Passthru
+
+    Context "-Name $FunctionName -Parameter a,b,c" {
+        $FunctionName = "TestFunction1"
+
+        it "should generate a function file with parameters" {
+            $FunctionPath = Join-Path $ModulePath "$FunctionName.ps1"
+            $FunctionTestsPath = Join-Path Tests "$FunctionName.Tests.ps1"
+
+            New-ForgeGeneratorFunction -Name $FunctionName -Parameter a1,b1,c1
+            $FunctionPath     | Should Exist
+            $FunctionPath     | Should Contain '\$a1,'
+            $FunctionPath     | Should Contain '\$b1,'
+            $FunctionPath     | Should Contain '\$c1'
+            $FunctionTestsPath | Should Exist
+        }
+    }
+
+    Context "-Name $FunctionName" {
+        It "should generate a function file" {
+            $FunctionPath | Should Exist
+            $FunctionPath | Should Contain "function $FunctionName"
+        }
+
+        It "should generate a test file" {
+            $FunctionTestsPath | Should Exist
+            $FunctionTestsPath | Should Contain "Describe `"$FunctionName`""
+        }
+
+        It "should add the function to the exported ones" {
+            $PsdPath = Join-Path $ModulePath "$ModuleName.psd1"
+            (Import-PowerShellDataFile $PsdPath)["FunctionsToExport"] | Should Be @($FunctionName)
+        }
+
+        BeforeEach {
+            New-ForgeGeneratorFunction -Name $FunctionName -Description BLABLA
+            $FunctionPath = (Join-Path $ModulePath "$FunctionName.ps1") 
+            $FunctionTestsPath = (Join-Path Tests "$FunctionName.Tests.ps1")
+        }
+    }
+
+    Context "Incorrect directory structure: no module dir" {
+        It "should fail if '<ModuleName>' directory does not exist" {
+            Remove-Item -Recurse $ModulePath
+            { 
+                New-ForgeGeneratorFunction -Name $FunctionName
+            } | Should Throw "Module directory '$ModulePath' does not exist"
+        }
+    }
+
+    Context "Incorrect directory structure: no Tests dir" {
+        It "should fail if 'Tests' directory does not exist" {
+            Remove-Item $TestsPath
+            { 
+                New-ForgeGeneratorFunction -Name $FunctionName
+            } | Should Throw "Test directory 'Tests' does not exist"
+        }
+    }
+
+    BeforeEach {
+        $Script:OldLocation = Get-Location
+        Set-Location $TestBase
+
+        $ModulePath = New-Item (Join-Path $TestBase $ModuleName) -Type Container
+        $TestsPath  = New-Item (Join-path $TestBase Tests) -Type Container
+        New-ModuleManifest "$ModuleName/$ModuleName.psd1"
+    }
+
+    AfterEach {
+        Set-Location $Script:OldLocation
+        Remove-Item -Recurse $TestBase/*
+   }
+}
